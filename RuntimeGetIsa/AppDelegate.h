cb2fb1e91d5c49ab3b044f700f57eb8ba26e60c7
Zhang <outsider.zhangr@gmail.com>
2018-04-28 10:22:55 +0800

    Initial Commit

---
+++ RuntimeGetIsa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RuntimeGetIsa
+//
+//  Created by sanjia on 2018/4/28.
+//  Copyright © 2018年 sanjia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RuntimeGetIsa/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RuntimeGetIsa
+//
+//  Created by sanjia on 2018/4/28.
+//  Copyright © 2018年 sanjia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
